bc708c1861e17c922fcac2b6697507ea2f3faf0f
D3Brooksster <137219076+D3Brooksster@users.noreply.github.com>
2025-05-17 23:33:45 +0100

    Update MessageViewer.tsx

--- src/components/recipient/MessageViewer.tsx
+++ src/components/recipient/MessageViewer.tsx
@@ -135,7 +135,7 @@ const MessageViewer: React.FC<MessageViewerProps> = ({
         const res = await reactionsApi.init(message.id, sessionId);
         activeReactionId = res.data.reactionId;
         setReactionId(activeReactionId);
-        onInitReactionId?.(activeReactionId as string, text);
+        onInitReactionId?.(activeReactionId);
       }
   
       // ✅ activeReactionId is now guaranteed to be string
