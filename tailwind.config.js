16aafcb1d729f4f0bff242d5b7fc713e1f0e9a9c
D3Brooksster <137219076+D3Brooksster@users.noreply.github.com>
2025-06-25 21:45:43 +0100

    Restore neutral palette using default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import forms from '@tailwindcss/forms';
 import aspectRatio from '@tailwindcss/aspect-ratio';
+import colors from 'tailwindcss/colors';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './src/styles/**/*.css'],
@@ -9,18 +10,8 @@ export default {
     extend: {
       colors: {
         neutral: {
-          50: '#F9FAFB',
-          100: '#F3F4F6',
-          200: '#E5E7EB',
-          300: '#D1D5DB',
-          400: '#9CA3AF',
-          500: '#6B7280',
-          600: '#4B5563',
-          700: '#374151',
-          800: '#1F2937',
+          ...colors.neutral,
           850: '#1C2526',
-          900: '#111827',
-          950: '#030712',
         },
         // Primary colors
         primary: {
