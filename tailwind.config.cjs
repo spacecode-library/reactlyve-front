704c39abdf29c2e4bdbb7be4422528873dae13b6
D3Brooksster <137219076+D3Brooksster@users.noreply.github.com>
2025-06-27 14:52:00 +0100

    fix dark theme variant

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {
       colors: {
