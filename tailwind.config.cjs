cfab37607102e5e4f02a270761c4d4d926d93ba5
D3Brooksster <137219076+D3Brooksster@users.noreply.github.com>
2025-06-27 12:52:46 +0100

    refactor: remove apply directives

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
-  applyComplexClasses: true,
+  experimental: {
+    applyComplexClasses: true,
+  },
   theme: {
     extend: {
       colors: {
