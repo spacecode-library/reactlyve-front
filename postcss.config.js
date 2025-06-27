9ea3c9d0fe93bfdfae0ffceba40a913e1438d827
D3Brooksster <137219076+D3Brooksster@users.noreply.github.com>
2025-06-27 08:32:26 +0100

    chore: update config references after rename

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    '@tailwindcss/postcss': { config: './tailwind.config.cjs' },
+    '@tailwindcss/postcss': { config: './tailwind.config.js' },
     autoprefixer: {},
   },
 }
